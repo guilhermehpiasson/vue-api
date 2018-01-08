7107c202e3a1023bb5e8f94c462e3c639f223d48
Guilherme Henrique Piasson <guilhermehpiasson@gmail.com>
2018-01-08 00:23:43 -0200

    Cors config

--- config/express.js
+++ config/express.js
@@ -1,19 +1,24 @@
 var express = require('express')
     ,app = express()
     ,bodyParser = require('body-parser')
+    ,cors = require('cors')
     ,routes = require('../app/routes');
+
+var app = express()
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+//   next();
+// });
 
 
 routes(app);
